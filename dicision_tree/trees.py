0831d697874275557d8ff8d563723af0308bad4c
dongyi.kim <waps12b@naver.com>
2017-06-05 15:21:57 +0900

    Create trees.py

---
+++ dicision_tree/trees.py
@@ -0,0 +1,95 @@
+from math import log
+import operator
+
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key]) / numEntries
+        shannonEnt -= prob * log(prob,2)
+    return shannonEnt
+
+
+def loadDataSet():
+    dataSet = []
+
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis+1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0]) - 1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet) / float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy
+        if infoGain > bestInfoGain :
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+def createTree(dataSet, labels, depthLimit = 10):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList) :
+        return classList[0]
+
+    if len(dataSet[0]) == 1 or depthLimit == 0:
+        return majorityCnt(classList)
+
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel:{}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels, depthLimit-1)
+    return myTree
+
+def predictClass(tree, features, labels):
+    #generate label to index mapping table
+    labelToIndex = {}
+    for i in range(0, len(labels)):
+        labelToIndex[labels[i]] = i
+
+    node = tree
+    while isinstance(node, dict):
+        label = list(node.keys())[0]
+        index = labelToIndex[label]
+        node = node[label][features[index]]
+
+    return node
+
+def saveTree(tree, fileName):
+    f = open(fileName, 'w')
+    f.write(tree)
+    f.close()
